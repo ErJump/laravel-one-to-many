93ce26bc8056e84946a1f841108b0e158e08e03a
ErJump <fedelijunior@gmail.com>
2022-09-21 15:33:33 +0200

    feat: :sparkles: Adds categories table migration + model + seed + adds column category_id to posts table

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\User;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,12 @@ class PostsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         $users = User::all();
+        $categories = Category::all();
 
         for($i = 0; $i < 50; $i++) {
             $newPost = new Post();
             $newPost->user_id = $faker->randomElement($users)->id;
-            $newPost->author = $faker->name();
+            $newPost->category_id = $faker->randomElement($categories)->id;
             $newPost->title = $faker->sentence(3);
             $newPost->post_content = $faker->text(500);
             $newPost->post_image = $faker->imageUrl(640, 480, 'animals', true);
